ffe608fc908602eb6e438cca86f304f9057d7652
Vijayasri <vijayasrikanniya120@gmail.com>
2026-08-29 12:34:18 +0530

    Configure backend for Render

--- backend/server.js
+++ backend/server.js
@@ -46,5 +46,5 @@ app.get("/products", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
